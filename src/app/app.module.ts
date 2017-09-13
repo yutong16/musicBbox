59ceb98498c5470ca2b39ada68f8d7abda0860d8
wupeng <494572656@qq.com>
2017-09-13 17:22:54 +0800

    结构布局

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HeadComponent } from './head/head.component';
+import { ContentComponent } from './content/content.component';
+import { FootComponent } from './foot/foot.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeadComponent,
+    ContentComponent,
+    FootComponent,
   ],
   imports: [
     BrowserModule
