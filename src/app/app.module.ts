7c0316d9343f065d264da2422bc9c3d13191759e
wupeng <494572656@qq.com>
2017-09-14 10:38:35 +0800

    修改配置

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,15 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+
+import {HttpModule} from '@angular/http';
+import {FormsModule} from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { HeadComponent } from './head/head.component';
 import { ContentComponent } from './content/content.component';
 import { FootComponent } from './foot/foot.component';
+import { NgZorroAntdModule} from 'ng-zorro-antd';
 
 @NgModule({
   declarations: [
@@ -14,7 +19,11 @@ import { FootComponent } from './foot/foot.component';
     FootComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    HttpModule,
+    BrowserAnimationsModule,
+    NgZorroAntdModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
