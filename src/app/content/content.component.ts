44d52b38f40d3a500b69003fe8c2c192aa07c2ba
wupeng <494572656@qq.com>
2017-09-15 17:32:22 +0800

    test

--- src/app/content/content.component.ts
+++ src/app/content/content.component.ts
@@ -1,3 +1,5 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -6,10 +8,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./content.component.css']
 })
 export class ContentComponent implements OnInit {
+  contactForm = {};
 
   constructor() { }
 
   ngOnInit() {
+  }
+  onChange(event:any) {
+    alert(event.target.value);
   }
 
 }
--- src/app/foot/foot.module.ts
+++ src/app/foot/foot.module.ts
@@ -4,13 +4,14 @@ import { FootComponent } from './foot.component';
 import { FormsModule } from '@angular/forms';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { NgZorroAntdModule } from 'ng-zorro-antd';
 
 @NgModule({
     declarations: [
         HighlightDirective,
         FootComponent,
     ],
-    imports: [ CommonModule ],
+    imports: [ CommonModule , NgZorroAntdModule],
     // 特性模块需要导出组件
     exports: [ FootComponent],
     providers: [],
